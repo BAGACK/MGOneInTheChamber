ec19f430d227719b220fe8a8b22a3415014dfe2b
mepeisen <mep_eisen@web.de>
2017-01-14 21:55:32 +0100

    v2

---
+++ v2/src/main/java/de/minigames/minigame/MGOneInTheChamberPlugin.java
@@ -0,0 +1,26 @@
+package de.minigames.minigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MGOneInTheChamberPlugin extends JavaPlugin
+{
+
+    public MGOneInTheChamberPlugin()
+    {
+        // TODO Put in some initialization code.
+    }
+
+    @Override
+    public void onEnable()
+    {
+        // TODO Put in your activation code.
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // TODO Put in your deactivation code.
+    }
+
+}
+
